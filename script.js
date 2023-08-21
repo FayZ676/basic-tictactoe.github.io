73634110516fbbef07042ef3bb1661bd33b60068
FayZ676 <faizififita1@gmail.com>
2023-08-21 16:34:35 -0700

    Add foundational objects.

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameBoard = (() => {
+  let _board = new Array(9);
+})();
+
+const Player = (sign) => {};
+
+// Use minimax algorithm for the logic
+const minimaxAiLogic = () => {};
+
+const gameController = (() => {})();
